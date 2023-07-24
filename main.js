dac989c7cdbaa87c714f6804dbaaf81068c5c43d
Beksulton Po'latov <beksulton753@gmail.com>
2023-07-24 19:40:36 +0500

    first commit

---
+++ main.js
@@ -0,0 +1,75 @@
+var natija = 129
+var kontrakt
+kontrakt >= 3000
+var kontrakt2
+kontrakt2 >= 2000
+
+if(natija < 80){
+alert("O'qishga kira olmadingiz")
+} 
+else
+if(natija <100){
+    alert("siz super kontrakt asosida o'qishga tavsiya qilindingiz")
+   kontrakt = prompt("kontrakt miqdori yiliga 3000 $ qancha pulingiz bor")
+    
+}
+if(kontrakt >= 3000){
+   kontrakt = alert("siz o'qishga qabul qilindingiz")
+}
+else 
+if(natija < 130){
+     alert("siz kontrakt asosida o'qishga qabul qilindingiz")
+    kontrakt2 = prompt("kontrakt miqdori yiliga 2000 $ qancha pulingiz bor")
+} 
+if(kontrakt2 >= 2000){
+    kontrakt2 = alert("Siz o'qishga qabul qilindingiz")
+}
+else 
+if(natija <= 180){
+    alert("Siz grant asosida o'qishga qabul qilindingiz")
+}
+else 
+if(natija >180){
+    alert("noto’g’ri qiymat kiritildi")
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+if(natija > 80){
+    alert(" siz super-kontrakt asosida o'qishga tavsiya qilindingiz")
+   kontrakt > prompt("kontrakt miqdori yiliga 3000 $ qancha pulingiz bor")
+}
+if(kontrakt ){
+   kontrakt = alert("Siz o'qishga qabul qilindingiz "  )
+} 
+
+else 
+if(natija > 100){
+    alert(" siz kontrakt asosida o'qishga qabul qilindingiz")
+   kontrakt2 = prompt("kontrakt miqdori yiliga 2000 $ qancha pulingiz bor")
+}
+if(kontrakt2 ){
+   kontrakt2 =  alert("Siz o'qishga qabul qilindingiz "  )
+} 
+else
+if(natija > 130){
+    alert("Siz grant asosida o'qishga qabul qilindingiz")
+}
+
+else
+if(natija > 180){
+    alert("noto’g’ri qiymat kiritildi")
+}*/
