8568c073759a6997966b17aa5d174c9739050d9c
Beksulton Po'latov <beksulton753@gmail.com>
2023-07-25 07:17:53 +0500

    two commit

--- main.js
+++ main.js
@@ -1,8 +1,11 @@
-var natija = 129
+var natija 
 var kontrakt
-kontrakt >= 3000
-var kontrakt2
-kontrakt2 >= 2000
+
+
+
+
+
+natija = prompt("Salom to'plagan balingizni kirirting")
 
 if(natija < 80){
 alert("O'qishga kira olmadingiz")
@@ -11,19 +14,21 @@ else
 if(natija <100){
     alert("siz super kontrakt asosida o'qishga tavsiya qilindingiz")
    kontrakt = prompt("kontrakt miqdori yiliga 3000 $ qancha pulingiz bor")
-    
+   if(kontrakt < 3000){
+     alert("siz o'qishga qabul qilinmadingiz")
+ } else if(kontrakt >= 3000){alert("siz o'qishga qabul qilindingiz")}
 }
-if(kontrakt >= 3000){
-   kontrakt = alert("siz o'qishga qabul qilindingiz")
-}
+
 else 
 if(natija < 130){
      alert("siz kontrakt asosida o'qishga qabul qilindingiz")
-    kontrakt2 = prompt("kontrakt miqdori yiliga 2000 $ qancha pulingiz bor")
+    kontrakt = prompt("kontrakt miqdori yiliga 2000 $ qancha pulingiz bor")
+
+    if(kontrakt < 2000){
+        kontrakt = alert("Siz o'qishga qabul qilinmadingiz")
+    } else if(kontrakt >= 2000){alert("siz o'qishga qabul qilindingiz")}
 } 
-if(kontrakt2 >= 2000){
-    kontrakt2 = alert("Siz o'qishga qabul qilindingiz")
-}
+
 else 
 if(natija <= 180){
     alert("Siz grant asosida o'qishga qabul qilindingiz")
@@ -47,29 +52,3 @@ if(natija >180){
 
 
 
-/*
-if(natija > 80){
-    alert(" siz super-kontrakt asosida o'qishga tavsiya qilindingiz")
-   kontrakt > prompt("kontrakt miqdori yiliga 3000 $ qancha pulingiz bor")
-}
-if(kontrakt ){
-   kontrakt = alert("Siz o'qishga qabul qilindingiz "  )
-} 
-
-else 
-if(natija > 100){
-    alert(" siz kontrakt asosida o'qishga qabul qilindingiz")
-   kontrakt2 = prompt("kontrakt miqdori yiliga 2000 $ qancha pulingiz bor")
-}
-if(kontrakt2 ){
-   kontrakt2 =  alert("Siz o'qishga qabul qilindingiz "  )
-} 
-else
-if(natija > 130){
-    alert("Siz grant asosida o'qishga qabul qilindingiz")
-}
-
-else
-if(natija > 180){
-    alert("noto’g’ri qiymat kiritildi")
-}*/
